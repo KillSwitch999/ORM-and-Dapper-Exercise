1f8ce2f5c6e6e2b45fad655c7ec7a96336179e48
George N. Gouvouniotis <ggouvounotis@proton.me>
2025-09-25 17:53:04 -0400

    First commit

---
+++ ORM-Dapper/Department.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ORM_Dapper
+{
+
+    public class Department
+    {
+        public Department()
+        {
+        }
+
+
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+    }
+
+}
